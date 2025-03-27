e23de8aa8c0a423aecfbde8bfd840a45ba000848
P_Preethika <preethika.punniyam@multicorewareinc.com>
2025-03-27 15:41:24 +0530

    ADDED CONFIG

---
+++ Files/PerfSettings8Mb.py
@@ -0,0 +1,23 @@
+{
+    "graphs": [
+        {
+            "vtcm_mb": 8,
+            "fp16_relaxed_precision": 0,
+            "graph_names": [
+                "slim_smol_main"
+            ],
+            "O": 3.0
+        }
+    ],
+    "devices": [
+        {
+            "dsp_arch": "v68",
+            "cores": [
+                {
+                    "perf_profile": "burst"
+                }
+            ],
+            "soc_id": 37
+        }
+    ]
+}
